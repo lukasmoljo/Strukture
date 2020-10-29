480722bd296df897fb65cdef54550cd48a95f979
lukasmoljo <73486463+lukasmoljo@users.noreply.github.com>
2020-10-29 11:28:44 +0100

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,27 @@
+typedef struct ucenik {
+    char ime[50], prez[50];
+    int bod;
+} ucenik;
+
+int main() {
+    FILE* f = fopen("file.txt", "r");
+    ucenik tmp;
+    int n;     
+    for (n = 0; fscanf(f, "%s %s %d", tmp.ime, tmp.prez, &tmp.bod) != 0; n++) {} 
+
+    int tot = 0; 
+
+    ucenik* ucenici = (ucenik*)malloc(n * sizeof(ucenik));
+    f = fopen("file.txt", "r"); 
+    for (int i = 0; i < n; i++) {
+        fscanf(f, "%s %s %d", ucenici[i].ime, ucenici[i].prez, &ucenici[i].bod);
+        tot += ucenici[i].bod;
+        
+    }
+
+    printf("apsolutni broj bodova je %d", tot);
+    printf("prosjecni broj bodova je %f", (float)tot / n);
+
+    return 0;
+
+}
